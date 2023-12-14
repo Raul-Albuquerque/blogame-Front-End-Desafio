fd353a8071c1275bb5deda1001d28cb7d367c53b
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2023-12-14 17:11:30 -0300

    feat: change metadata

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Blogame - Desafio Front-End',
+  description: 'Front-End Challenge',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-br">
+      <body className="bg-dark30">{children}</body>
+    </html>
+  )
+}
